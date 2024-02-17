744aef8d0bcacc3e6bc4e8ffc1755963aeba39b4
eddyyucca <eddyyucca@gmail.com>
2024-02-17 08:00:30 +0800

    sidebar

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,7 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('template/header');
+		$this->load->view('template/sidebar');
 		$this->load->view('dashboard/dashboard_index');
 		$this->load->view('template/footer');
 	}
